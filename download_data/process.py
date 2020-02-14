c4502d11cc2aec88d5efda711f2fd5975fc012b7
Xinyu Zhao <xinyuzhao@utexas.edu>
2020-02-13 19:59:45 -0600

    unzip and split files from gigaword, organized into folders

--- download_data/process.py
+++ download_data/process.py
@@ -4,8 +4,8 @@ import re
 
 from bs4 import BeautifulSoup
 
-DATA_PATH = "../../gigaword_eng_5/data/"
-folders = ["apw_eng"] 
+DATA_PATH = "../../../gigaword_eng_5/data/"
+folders = ["nyt_eng"] 
 OUTPUT_PATH = "./data_processed/"
 
 if not os.path.exists(OUTPUT_PATH):
@@ -15,10 +15,15 @@ for folder in folders:
 	if not os.path.exists(OUTPUT_PATH + folder):
 		os.mkdir(OUTPUT_PATH + folder)
 	for filename in os.listdir(DATA_PATH + folder):
+		print(filename)
+		OUT_DIR = os.path.join(OUTPUT_PATH, folder, filename[:-3])
+		if not os.path.exists(OUT_DIR):
+			os.mkdir(OUT_DIR)
 		file = gzip.open(os.path.join(DATA_PATH + folder, filename))
 		soup = BeautifulSoup(file, "html.parser")
 		for doc in soup("doc"):
 			docname = doc.attrs['id']
+			print(docname)
 			paragraphs = []
 			for p in doc("p"):
 				para = p.get_text().strip()
@@ -27,7 +32,7 @@ for folder in folders:
 				paragraphs.append(para)
 			final = " ".join(paragraphs)
 			if final.strip() == "":
-				continue
-			f = open(os.path.join(OUTPUT_PATH, folder, docname), "w")
+				continue 
+			f = open(os.path.join(OUTPUT_PATH, folder, filename[:-3], docname), "w")
 			f.write(final)
 			f.close()
