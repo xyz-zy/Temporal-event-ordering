f9f7105b5c05042e470e388b5594f7a9cd3f3f3c
Xinyu Zhao <xinyuzhao@utexas.edu>
2020-05-17 01:06:33 -0500

    fixes

--- download_data/create_file.py
+++ download_data/create_file.py
@@ -6,19 +6,31 @@ from bs4 import BeautifulSoup
 parser = argparse.ArgumentParser(description="parse examples from xml")
 parser.add_argument('--input_dir', type=str, help="directory containing xml")
 parser.add_argument('--out', type=str, help="output filename")
+parser.add_argument('--out_dir', type=str, help="output directory")
 args = parser.parse_args()
 
 input_folder = args.input_dir
+output_folder = args.out_dir
 
-f = open("extracted/" + args.out, "w")
+if not input_folder or not output_folder:
+  print("please specify args")
+  exit()
+
+if not os.path.exists(output_folder):
+  os.makedirs(output_folder)
+
+f = open(output_folder + "/" + args.out, "w")
 for filename in os.listdir(input_folder):
+	full_path = os.path.join(input_folder, filename)
+	if not os.path.isfile(full_path) or not filename.endswith("xml"):
+		continue
 	print(filename)
 	soup = BeautifulSoup(open(os.path.join(input_folder, filename)), "html.parser")
 	tlinks = [x for x in soup.findAll('tlink')]
 	event_pairs = []
 	events = []
 	for t in tlinks:
-		if t.attrs["type"] == "ee":
+		if t.attrs["type"] == "ee" and (t.attrs["origin"] == "TimeTimeSieve" or t.attrs["origin"] == "AdjacentVerbTimex"):
 			event_pairs.append((t.attrs["event1"], t.attrs["event2"], t.attrs["relation"]))
 			events.append(t.attrs["event1"])
 			events.append(t.attrs["event2"])
@@ -26,13 +38,25 @@ for filename in os.listdir(input_folder):
 	for entry in soup.findAll("entry"):
 		if str(type(entry)) == "<class 'bs4.element.Tag'>":
 			events_entry = {x.attrs["eiid"]: (x.attrs["string"], int(x.attrs['offset'])-1) for x in entry.findAll("event")}
-			sentence = entry.find("sentence").text
-			tags = [''] * len(sentence.split())
+			token_group = entry.find("tokens")
+			tokens = []
+			for tok in token_group.findAll("t"):
+				tok_text = tok.text.replace(" ", "")
+				tok_text = tok_text[3:-3]
+				tokens.append(tok_text)
+			sentence = " ".join(tokens)
+			tags = [''] * len(tokens)
+			#print(len(tokens))
 			for x in entry.findAll("event"):
 				tags[int(x.attrs['offset'])-1] = x.attrs['eiid']
 			for x in entry.findAll("timex"):
+				offset = int(x.attrs['offset'])
 				for i in range(int(x.attrs['length'])):
-					tags[int(x.attrs['offset'])-1+i] = x.attrs['tid']
+					if offset-1+i >= len(tags):
+						print(offset, x.attrs['length'], x.attrs['text'])
+
+					tags[offset-1+i] = x.attrs['tid']
+					#print(tokens[offset-1+i])
 			event_list = set(events).intersection(set(events_entry.keys()))
 			if len(event_list) > 0:
 				for e in event_list:
